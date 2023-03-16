3e33c421b517c69b8b8efa1f3e805455c3625fef
Divine <ceeflames@yahoo.com>
2023-03-16 09:53:10 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ * Divine is enjoying Alx
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, --date=\" 2015-10-19\"", 59);
+	return (1);
+
+}
