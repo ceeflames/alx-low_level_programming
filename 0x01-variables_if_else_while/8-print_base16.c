6f8c2110af0683d9a93d148cdcd6e4ba3fb551b8
Divine <ceeflames@yahoo.com>
2023-03-17 11:00:28 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	char i;
+
+	for (n = 0; n < 10; i++)
+		putchar((n % 10) + '0');
+
+	for (i = 'a'; i <= 'f'; a++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}
