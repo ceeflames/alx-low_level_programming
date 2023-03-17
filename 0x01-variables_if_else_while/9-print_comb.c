f3cc9d213be0c5b056863c84905cae94105d34a0
Divine <ceeflames@yahoo.com>
2023-03-17 11:25:33 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,11 +9,14 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n++)
+	for (n = 0; n <= 9;  n++)
+	{
 		putchar((n % 10) + '0');
-	if (n == 9)
-		continue;
-	putchar(',');
+		if (n == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
 	putchar('\n');
 
 	return (0);
