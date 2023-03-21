db1bdc5605527ebe83ade6ef02e7d3795ad6a563
Divine <ceeflames@yahoo.com>
2023-03-21 13:57:24 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letters = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+    	return (0);
+}
