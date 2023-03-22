03c85fd3d9a92bc595d6811105f409f716184f68
Divine <ceeflames@yahoo.com>
2023-03-21 22:09:45 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - list all natural numbers < 1024
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
