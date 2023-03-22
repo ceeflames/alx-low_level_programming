30b4ae397442b4ec5a266ccb5a46ae349101cf73
Divine <ceeflames@yahoo.com>
2023-03-21 22:16:07 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - prints first 50 fibonacci numbers
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int count;
+	unsigned long f1 = 0, f2 = 1, sum;
+
+	for (count = 0; count < 50; count++)
+	{
+		sum = f1 + f2
+			printf("%lu", sum);
+
+		f1 = f2;
+		f2 = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+
+}
