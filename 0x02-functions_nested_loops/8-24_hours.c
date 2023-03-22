cd740a7d497ca0da4bdcf75be30a61c4bc76241a
Divine <ceeflames@yahoo.com>
2023-03-21 21:38:02 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,7 +12,7 @@ void jack_bauer(void)
 
 	for (hour = 0; hour <= 23; hour++)
 	{
-		for (min = 0; mmin <= 59; min++)
+		for (min = 0; min <= 59; min++)
 		{
 			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
