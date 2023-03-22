7866fa762a781ac79e5687dec46449446c394d56
Divine <ceeflames@yahoo.com>
2023-03-22 12:58:40 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,49 +6,38 @@
  *
  * Return: Always 0 (success)
  */
-
 int main(void)
 {
-	int count;
-	unsigned long f1 = 0, f2 = 1, sum;
-	unsigned long f1_h1, f1_h2, f2_h1, f2_h2;
-	unsigned long h1, h2;
+	unsigned long int i, j, k, j1, j2, k1, k2;
 
-	for (count = 0; count < 92; count++)
+	j = 1;
+	k = 2;
+
+	printf("%lu", j);
+
+	for (i = 1; i < 91; i++)
 	{
-		sum = f1 + f2;
-		printf("%lu, ", sum);
-
-		f1 = f2;
-		f2 = sum;
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
 	}
 
-	f1_h1 = f1 / 10000000000;
-	f2_h1 = f2 / 10000000000;
-	f1_h2 = f1 % 10000000000;
-	f2_h2 = f2 % 10000000000;
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
 
-
-	for (count = 93; count < 99; count++)
+	for (i = 92; i < 99; ++i)
 	{
-		h1 = f1_h1 + f2_h1;
-		h2 = f1_h2 + f2_h2;
-		if (f1_h2 + f2_h2 > 9999999999)
-		{
-			h1 += 1;
-			h2 %= 10000000000;
-		}
-
-		printf("%lu%lu", h1, h2);
-		if (count != 98)
-			printf(", ");
-
-		f1_h1 = f2_h1;
-		f1_h2 = f2_h2;
-		f2_h1 = h1;
-		f2_h2 = h2;
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
 	}
+
 	printf("\n");
+
 	return (0);
 }
-
