6f2b31a53012f34570db2710aeb9d489f17bf865
Divine <ceeflames@yahoo.com>
2023-03-23 22:27:03 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies twi integeres
+ * @a: integer 1
+ * @b: integer 2
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	printd("%dn", mul(a, b));
+
+	return (0);
+}
