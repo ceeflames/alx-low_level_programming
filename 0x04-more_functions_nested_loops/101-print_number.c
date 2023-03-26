ed3b8446c806053bc250a067ebcae80f78125ac2
Divine <ceeflames@yahoo.com>
2023-03-26 15:14:52 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Alwauys 0.
+ */
+
+void print_number(int n)
+{
+	unsigned int c = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		c = -c;
+	}
+
+
+}
+
