24000d63a62b6be81b35d56daeda645bae5e5a20
Divine <ceeflames@yahoo.com>
2023-03-28 13:30:19 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - keygen
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int myrand;
+	int count;
+	int sum;
+
+	srand(time(NULL));
+	for (count = 0, sum = 2772; sum > 122; count++)
+	{
+		myrand = (rand() % 125) + 1;
+		printf("%c", myrand);
+		sum -= myrand;
+	}
+	printf("%c", sum);
+
+	return (0);
+}
