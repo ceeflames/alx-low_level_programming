58d580289ea3d10f998880d21d697e768aef219c
Divine <ceeflames@yahoo.com>
2023-03-29 12:26:51 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,13 +10,13 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-  int i;
-  int dest_len = strlen(dest);
+	int i;
+	int dest_len = strlen(dest);
 
-  for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 
-    dest[dest_len + i] = src[i];
-  dest[dest_len + i] = '\0';
+	dest[dest_len + i] = src[i];
+	dest[dest_len + i] = '\0';
 
-  return (dest);
+	return (dest);
 }
