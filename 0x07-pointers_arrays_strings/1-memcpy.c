172d448aba6f39fa72abf0bff16cea36271c4290
Divine <ceeflames@yahoo.com>
2023-04-03 16:26:24 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * *_memcpy - function that copies memeory area
+ * @dest: destination
+ * @src: source
+ * @n: number of bytes
+ * Return: dest;
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (; i <= n; i++)
+		dest = src;
+	return (dest);
+}
