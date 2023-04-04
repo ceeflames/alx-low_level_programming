ff212e9fd546dbb70cc904fbdc1504ea7b3b81a2
Divine <ceeflames@yahoo.com>
2023-04-04 02:38:02 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * *_strpbrk - Function that searches a string for any
+ * of a set of bytes.
+ * @s: constant
+ * @accept: constant
+ * @t: temp variable
+ * Return: s.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	const char *t;
+
+	t = accept;
+
+	if (*s == '\0' || *accept == '\0')
+	{
+		return ('\0');
+	}
+
+	while (*s != '\0')
+	{
+
+		while (*t != '\0')
+		{
+			if (*s == *t)
+			{
+				return (s);
+			}
+			t++;
+		}
+		s++;
+	}
+	return ('\0');
+
+	return (0);
+}
