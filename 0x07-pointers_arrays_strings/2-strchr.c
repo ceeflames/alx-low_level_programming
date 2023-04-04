0572c5ffeca9085fb0ad13d3fceae29d5117b3ff
Divine <ceeflames@yahoo.com>
2023-04-03 17:15:45 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - function that locates a character in string
+ * @s: constant
+ * @c: character
+ * Return: NULL.
+ */
+char *_strchr(char *s, char c)
+{
+	while (s != '\0')
+	{
+		if (*s == c)
+		{
+			return ((char *) s);
+		s++;
+		}
+	}
+	return (NULL);
+}
