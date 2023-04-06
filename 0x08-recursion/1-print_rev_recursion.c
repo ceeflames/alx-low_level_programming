0668ca6d0f42869e4967999eb0665246931779ae
Divine <ceeflames@yahoo.com>
2023-04-06 00:59:02 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,18 +6,10 @@
  */
 void _print_rev_recursion(char *s)
 {
-	int i, n;
-
-	n = 0;
-
-	while (s[n] != '\0')
+	if (*s)
 	{
-		n++;
+		_print_rev_recursion(s + 1);
+		putchar(*s);
 	}
-	for (i = n - 1; i >= 0; i--)
-	{
-		_putchar(s[i]);
-	}
-	_putchar('\n');
 
 }
