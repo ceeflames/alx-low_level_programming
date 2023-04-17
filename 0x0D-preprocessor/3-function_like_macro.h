4ac66665421de20fe4c380b0128bf98507120ca8
Divine <ceeflames@yahoo.com>
2023-04-17 16:38:43 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,8 +1,6 @@
-#ifndef _FUNCTION_LIKE_MACRO_H_
-#define _FUNCTION_LIKE_MACRO_H_
+#ifndef _3 _FUNCTION_LIKE_MACRO_H_
+#define _3_FUNCTION_LIKE_MACRO_H_
 
 #define ABS(x) ((x) < 0 ? (-(x)): (x))
 
-#endif /** 
-#ifndef _FUNCTION_LIKE_MACRO_H_
-/
+#endif 
