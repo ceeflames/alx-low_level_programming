4b65c33f74139d288446d1e5427781dade32c450
Divine <ceeflames@yahoo.com>
2023-04-17 19:25:35 -0700

    5-free__dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,4 +15,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
+	dog = malloc(sizeof(struct dog));
+	if (dog == NULL)
+		return (NULL);
+	for (i = 0; name[i]; i++);
+
 }
