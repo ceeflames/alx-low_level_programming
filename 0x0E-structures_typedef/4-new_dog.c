ceba6218ffb975e00a6ff20ec5fafd950700aa7a
Divine <ceeflames@yahoo.com>
2023-04-18 14:46:25 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -5,7 +5,7 @@
  * *new_dog - function that creates a new dog
  * @name: pointer to the name
  * @age: second member
- * @owner: ointer to owner
+ * @owner: pointer to owner
  * Return: NULL (failure)
  */
 dog_t *new_dog(char *name, float age, char *owner)
@@ -20,4 +20,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	for (i = 0; name[i]; i++);
 
+	dog->name = malloc(sizeof(char) * i);
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	for (k = 0; k < i; k++)
+		dog->name[k] = name[k];
+	dog->age = age;
+	for (j = 0; owner[j]; j++)
+		dog->owner = malloc(sizeof(char) * j);
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (k = 0; k < j; k++)
+		dog->owner[k] = owner[k];
+	return (dog);
 }
