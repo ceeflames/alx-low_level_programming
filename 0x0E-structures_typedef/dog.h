d1bf35901dbd2be3259e251a498a32986fdf2a7f
Divine <ceeflames@yahoo.com>
2023-04-17 18:40:03 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_
+#define _DOG_
+
+/**
+ * struct dog - dog structure
+ * @name:member one
+ * @age: member two
+ * @owner: member three
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
