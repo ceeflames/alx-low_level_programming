b52f0e37a77540c0903f0e67b4c7c507ac779fe6
Divine <ceeflames@yahoo.com>
2023-04-20 12:03:00 -0700

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,16 @@
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = atoi(s);
+
+	if (s)
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * op_add - addition
+ * @a: integer one
+ * @b: integer two
+ * Return: 0
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtraction
+ * Return: 0
+ */
+
+int op_sub(int a, int b);
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication
+ * Return: 0
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Division
+ * Return: 0
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Modulos
+ * Return: 0
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
