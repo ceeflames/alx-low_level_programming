c06c7af51d543bae2f1eac4aeac9b7825ba15adb
Divine <ceeflames@yahoo.com>
2023-04-30 09:07:48 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -13,11 +13,11 @@ size_t print_listint(const listint_t *h)
 	size_t Number_of_nodes = 0;
 	const listint_t *h2;
 
-	h2 = h;
 
-	for (h2 = h; h2 != NULL; Number_of_nodes++, h2 = h2->next)
-	printf("%d\n", h2->n);
-
-
+	for (h2 = h; h2 != NULL; Number_of_nodes++)
+	{
+		printf("%d\n", h2->n);
+		h2 = h2->next;
+	}
 	return (Number_of_nodes);
 }
---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * listint_len - function that returns the number of
+ * elements in a linked list.
+ * @h: pointer to the elements
+ *
+ * Return: Number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+
+	return (No_of_element);
+}
