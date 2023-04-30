40ec8298723c612d0f1647bbfea4d6ec8e83091f
Divine <ceeflames@yahoo.com>
2023-04-30 16:20:07 -0700

    2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -14,12 +14,12 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	if (head == NULL)
 		return (NULL);
-	p = malloc (sizeof(listint_t));
+	p = malloc(sizeof(listint_t));
 	if (p == NULL)
 		return (NULL);
 	p->n = n;
 	p->next = *head;
 	*head = p;
-	free(p);
 	return (p);
+	free(p);
 }
