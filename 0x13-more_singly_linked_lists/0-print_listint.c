7ed12f3741dc9642f6bb1dd031a476d0d3f9a48f
Divine <ceeflames@yahoo.com>
2023-04-30 05:22:07 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,7 +11,8 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t Number_of_nodes = 0;
-	const listint_t *h2;	
+	const listint_t *h2;
+
 	h2 = h;
 
 	for (h2 = h; h2 != NULL; Number_of_nodes++, h2 = h2->next)
