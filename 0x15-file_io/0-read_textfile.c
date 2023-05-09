acce0babece7e38c5a05867e9781eb43d7b7b7f4
Divine <ceeflames@yahoo.com>
2023-05-09 05:00:15 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * read_textfile - function that reads a text file and prints
+ * to the POSIX standard output.
+ * @filename: pointer to that filename
+ * @letters: number of letters to read and print
+ *
+ * Return: 0 Always
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *filename;
+	size_t letters[];
+
+	filename = fopen(file
+	fclose(filename);
+	return (0);
+}
