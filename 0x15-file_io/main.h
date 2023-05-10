42fb60e48f03e462bc33bab28be6db85a649b51f
Divine <ceeflames@yahoo.com>
2023-05-10 02:16:21 -0700

    0-read_textfile.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
@@ -7,4 +15,4 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
 
-#endif /** end of main.h /
+#endif
