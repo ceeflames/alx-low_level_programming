37cf79465ecdffd670fbe3a5d16f7331aa2014f2
Divine <ceeflames@yahoo.com>
2023-05-10 14:43:12 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: name of file to be created
+ * @text_content: null terminator string to write to the file
+ *
+ * Return: 1 (successfull) || -1 (failure)
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fildes, i, st;
+
+	if (filename == NULL)
+		return (-1);
+
+	fildes = fopen(filename, O_CREAT | O_WRONLY | O_TRUNC);
+	if (fildes == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		for (i = 0; text_content[i] != '\0'; i++);
+		st = write(fildes, text_content, i);
+		if (st == -1)
+			return (-1);
+	}
+
+	close(fildes);
+
+	return (1);	
+}
