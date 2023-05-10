19dbab5f39af26b429d04936e043239806dd3aa4
Divine <ceeflames@yahoo.com>
2023-05-10 04:32:15 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,7 +6,7 @@
  * @filename: pointer to that filename
  * @letters: number of letters to read and print
  *
- * Return: 0 Always
+ * Return: 0 (failure) || number of letters read and printed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -16,23 +16,25 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	o = open(filename, O_RDWR);
-	if (o == -1)
-		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
-		free(buffer);
 		return (0);
 	}
+	o = open(filename, O_RDWR);
 	r = read(o, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
 
-	if (r == -1 || w == -1 || w != r)
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		w++;
 		return (0);
+	}
+
 	free(buffer);
 
 	close(o);
-	return (0);
+	return (w);
 }
