841961467cd9af524aee4f9cbc3bc29ee185454f
Divine <ceeflames@yahoo.com>
2023-05-10 02:19:22 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,7 +20,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (o == -1)
 		return (0);
 
-	buffer = malloc (sizeof(char) *letters);
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
 		free(buffer);
