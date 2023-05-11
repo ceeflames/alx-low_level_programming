718208df90cae64278b410c1d0b7de1a9f862bc5
Divine <ceeflames@yahoo.com>
2023-05-11 10:30:36 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fildes = fopen(filename, O_CREAT | O_WRONLY | O_TRUNC);
+	fildes = open(filename, O_CREAT | O_WRONLY | O_TRUNC | S_IRUSR);
 	if (fildes == -1)
 		return (-1);
 
