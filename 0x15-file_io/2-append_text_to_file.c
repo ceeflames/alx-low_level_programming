12997f96a3dd2121a7eb333e5d1138bfa35d2256
Divine <ceeflames@yahoo.com>
2023-05-11 14:16:45 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,17 +10,23 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, i;
+	int fd, i, st;
 	
 	if (filename == NULL)
 		return (-1);
-
-	fd = open(filename, "a");
 	
-	if (text_content)
-	{
-		for (i = 0; i < text_content; i++);
-	}
+	if (text-content == NULL)
+		return (1);
+
+	fd = open(filename, O_APPEND | O_WRONLY);
+	if (fd == -1)
+		return (-1);
+	
+	for (i = 0; i < text_content; i++);
+	
+	st = write(fd, text_content, i);
+	if (st == -1)
+		return (-1);
 
 	close(fd);
 	return (1);
