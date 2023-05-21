1d50acdf701732cf1a71a47e2fca454105d80ed0
Divine <ceeflames@yahoo.com>
2023-05-21 15:43:33 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,14 +15,22 @@ int main(int argc, char *argv[])
 
 	mode = S_IRUSR | S_IWUSR | S_IWGRP | S_IROTH;
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file _to\n"),exit(97);
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file _to\n");
+		exit(97);
+	}
 	fd1 = open(argv[1], O_RDONLY);
 	if (fd1 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, mode);
 	if (fd2 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]), exit (99);
-	
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
+		exit(99);
+	}
 	do {
 		st1 = read(fd1, buffer, MAXSIZE);
 		if (st1 == -1)
@@ -40,18 +48,20 @@ int main(int argc, char *argv[])
 			}
 		}
 	}
-	while (st1 > 0);
-	st1 = close(fd1);
-	if (st1 == -1)
+	while (st1 > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
-		exit(100);
-	}
-	st2 = close(fd2);
-	if (st2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
-		exit(100);
+		st1 = close(fd1);
+		if (st1 == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
+			exit(100);
+		}
+		st2 = close(fd2);
+		if (st2 == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+			exit(100);
+		}
 	}
 
 
