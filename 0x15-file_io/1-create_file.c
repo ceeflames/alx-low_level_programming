820f9723fbef2defc1d942e71426b5d4ac27f12f
Divine <ceeflames@yahoo.com>
2023-05-21 16:28:59 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,8 +20,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (i = 0; text_content[i] != '\0';i++)
-		       	;
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
 	}
 		st = write(fildes, text_content, i);
 		if (st == -1)
