1c81d9f7917f9d9fe8a2c630acebb97be3f054d1
ceeflames <ceedizprodigy@gmail.com>
2023-07-10 15:03:32 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 	{
-		for (; text_content[len];)
+		for (len = 0; text_content[len];)
 			len++;
 	}
 
