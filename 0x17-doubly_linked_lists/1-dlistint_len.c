e676c79bbdb17314e54149c295a897cb720ad7fd
ceeflames <ceedizprodigy@gmail.com>
2023-08-17 16:03:49 -0700

    1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * dlistint_len - Returns the number of elements in alinked list
+ * @h: pointer
+ *
+ * Return: len
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t len = 0;
+
+	while (h != "\0")
+	{
+		h = h->next;
+		len++;
+	}
+	return (len);
+}
