c5c831e34f84b9dc4fd2c350d2511da324d76a9b
ceeflames <ceedizprodigy@gmail.com>
2023-08-17 16:13:11 -0700

    1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -7,12 +7,15 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t len = 0;
+	size_t len_list = 0;
+
+	if (h == NULL)
+		return (0);
 
 	while (h != NULL)
 	{
 		h = h->next;
-		len++;
+		len_list++;
 	}
-	return (len);
+	return (len_list);
 }
