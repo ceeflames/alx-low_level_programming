380050db2f0b90ab87abfcd728f9014daf0f3b29
ceeflames <ceedizprodigy@gmail.com>
2023-08-18 01:29:30 -0700

    4-free_dlistint.c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_dlistint - Frees a list
+ * @head: ptr to the list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *ptr = head;
+
+	while (ptr != NULL)
+	{
+		dlistint_t *temp = ptr;
+		ptr = ptr->next;
+		free(temp)
+	}
+
+}
