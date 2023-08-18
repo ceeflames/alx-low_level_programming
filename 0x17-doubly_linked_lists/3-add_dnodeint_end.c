89b686c6e50d5b08618460b30339d4254e4b5b0c
ceeflames <ceedizprodigy@gmail.com>
2023-08-18 01:16:23 -0700

    3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -12,14 +12,22 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	if (add_end == NULL)
 		return (NULL);
+	
 	add_end->n = n;
-	add_end->prev = NULL;
-	add_end->next = *head;
+	add_end->next = NULL;
 
-	if (*head != NULL)
-		(*head)->next = add_end;
-
-	*head = add_end;
-
+	if (*head == NULL)
+	{
+		add_end->prev = NULL;
+		*head = add-end;
+	}
+	else
+	{
+		dlistint_t *curr = *head;
+		while (curr->next != NULL)
+			curr = curr->next;
+		curr->next = add_end;
+		add_end->prev = curr;
+	}
 	return (add_end);
 }
