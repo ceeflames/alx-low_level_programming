f27e4de10efd3d0c3a0a7be31e1f3764258fa599
ceeflames <ceedizprodigy@gmail.com>
2023-09-11 11:29:24 -0700

    hash

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,8 +15,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	hash_table = malloc(sizeof(hash_table_t));
-	if (hash_table == NULL)
-		return (NULL);
+	if (!hash_table)
+		return (0);
 
 	hash_table->size = size;
 	hash_table->array = malloc(sizeof(hash_node_t *) * size);
