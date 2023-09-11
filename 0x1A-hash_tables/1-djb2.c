0c358fdb725e26c85a8f2268ab88565264200fda
ceeflames <ceedizprodigy@gmail.com>
2023-09-11 14:10:33 -0700

    dj2b

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -8,12 +8,12 @@
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
-	int i;
+	int c;
 
 	hash = 5381;
-	while ((i = 8str++))
+	while ((c = *str++))
 	{
-		hash = ((hash << 5) + hash) + i;
+		hash = ((hash << 5) + hash) + c;/* hash * 33 + c */
 	}
 	return (hash);
 }
